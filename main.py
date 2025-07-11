6729e77b2ea1ed8ddf3924dbf01987bbcf28d504
JpLyg <japjolla@gmail.com>
2025-07-10 19:10:31 -0700

    saving prompts on a list now

--- main.py
+++ main.py
@@ -9,6 +9,11 @@ api_key = os.environ.get("GEMINI_API_KEY")
 from google import genai
 client = genai.Client(api_key=api_key)
 
+from google.genai import types
+
+
+
+
 
 def main():
     print("Hello from ai-agent!")
@@ -20,13 +25,21 @@ def main():
         SystemExit(1)
         sys.exit(1)
     else:
-        ai_function(sys.argv[1])
+
+        messages = [
+             types.Content(role="user", parts=[types.Part(text=sys.argv[1])]),
+            ]
+        
+
+
+
+        ai_function(sys.argv[1],messages)
     
 
-def ai_function(string):
+def ai_function(string,messages):
 
     response = client.models.generate_content(
-    model='gemini-2.0-flash-001', contents=string
+    model='gemini-2.0-flash-001', contents=messages
         )
     print(response.text)
     print("Prompt tokens:",response.usage_metadata.prompt_token_count)
